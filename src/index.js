2a337c47b67e72dac8e5a15c54d52d75b79ddf8e
Mahissara <mahi@Spitfire.local>
2019-09-28 10:48:53 +0530

    nodejs fizzbuzz

--- src/index.js
+++ src/index.js
@@ -1,5 +1,78 @@
+'use strict';
+
 const { getRandomWordSync, getRandomWord } = require('word-maker');
+const fs = require('fs');
+const PATH = require("path");
+const outFile = PATH.resolve(__dirname + '/../output.txt');
 
-console.log('It works!');
+async function runExercises() {
+	try{
+        await 
+        exercise1();
+		exercise2();
+		await exercise3();
+	} catch(err) {
+		console.log(err);
+	}
+}
 
-// YOUR CODE HERE
+
+/**
+Task 1 
+Print numbers from 1 to 100 with a random word
+*/
+async function exercise1() {
+
+	for(let i = 1; i <= 100; i++) {
+        let word = await getRandomWordSync();
+		await writeToFile(`${i}: ${word}`);
+    }
+}
+
+/**
+Task 2 
+Modified function for
+Print numbers from 1 to 100
+multiples of three, print "Fizz"
+multiples of five, print "Buzz"
+multiples of three and five, print "FizzBuzz"
+*/
+async function exercise2() {
+	for(let i = 1; i <= 100; i++) {
+		let word = await getRandomWordSync();
+        if(i%3 === 0) word = 'Fizz';
+        if(i%5 === 0) word = 'Buzz';
+        if(i%15 === 0) word = 'FizzBuzz';
+        await writeToFile(`${i}: ${word}`);
+	}
+}
+
+/**
+ * Task 3
+ */
+function exercise3() {
+
+	for(let i = 1; i <= 100; i++) {
+		getRandomWord({ withErrors: true })
+		.then(function(word) {
+			writeToFile(`${i}: ${word}`);
+		})
+		.catch(function(err) {
+			writeToFile(`${i}: It shouldn't break anything!`);
+		})
+	}
+}
+
+/**
+ * Write each line into text file
+ * @param {*} line 
+ */
+function writeToFile(line) {
+    
+    line+= '\n';
+	fs.appendFile(outFile, line, function (err) {
+		if (err) throw err;
+	});
+}
+
+runExercises();
